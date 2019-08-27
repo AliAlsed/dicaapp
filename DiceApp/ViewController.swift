e2058d2ef8853bbe50b808236ce0c639816ce008
Ali Alsed <alialsed19961969@gmail.com>
2019-08-27 10:42:23 +0300

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Ali Almosoy on 8/27/19.
+//  Copyright © 2019 Ali Almosoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
