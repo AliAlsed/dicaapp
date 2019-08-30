22ec956119bc772e7f0ffe4efbd07dd492b600c2
Ali Alsed <alialsed19961969@gmail.com>
2019-08-30 13:15:24 +0300

    ccreate full dice app

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let diceArray = [ "dice1","dice2","dice3","dice4","dice5","dice6"]
+    var diceIndex1: Int = 0;
+    var diceIndex2:Int = 0;
+    @IBOutlet weak var dice2: UIImageView!
+    
+    @IBOutlet weak var dice1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDiceImages()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButton(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    func updateDiceImages(){
+        diceIndex1 = Int.random(in: 0 ... 5)
+        diceIndex2 = Int.random(in: 0 ... 5)
+        dice1.image = UIImage(named: diceArray[diceIndex1])
+        dice2.image = UIImage(named: diceArray[diceIndex2])
+    }
 }
 
